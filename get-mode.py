c1bea785e8cb92313ea5af8b006a164e54944b0b
aflynn0309 <107883034+aflynn0309@users.noreply.github.com>
2022-08-20 11:34:31 -0400

    Update get-mode.py

--- get-mode.py
+++ get-mode.py
@@ -1,3 +1,3 @@
-list = list("enter list:")
+list = 1, 2, 3, 2, 5, 6, 0, 1, 2, 6, 3 #can be replaced with any list including words/letters as long as its in the same format
 print("Mode of List A is % s" % (max(set(list), key = list.count)))
 input('press enter to exit')
